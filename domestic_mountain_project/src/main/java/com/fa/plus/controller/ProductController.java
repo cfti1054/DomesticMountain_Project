7922ded7a54ba5d4f4572250076cc62df28578f4
user <user@DESKTOP-M43I481>
2023-12-16 13:51:49 +0900

    고쳐져라

--- domestic_mountain_project/src/main/java/com/fa/plus/controller/ProductController.java
+++ domestic_mountain_project/src/main/java/com/fa/plus/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/product/*")
-public class productController {
+public class ProductController {
 	
 	@Autowired
 	// private MemberService service;
