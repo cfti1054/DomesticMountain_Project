c18f9e769dc71ff707fb4b3b12906acd48398574
4reoi <4reoi@DESKTOP-NCAL65T>
2023-12-28 14:40:30 +0900

    공지사항 - 김혁

--- domestic_mountain_project/src/main/java/com/fa/plus/controller/NoticeController.java
+++ domestic_mountain_project/src/main/java/com/fa/plus/controller/NoticeController.java
@@ -1,16 +1,180 @@
 package com.fa.plus.controller;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fa.plus.common.FileManager;
+import com.fa.plus.common.MyUtil;
+import com.fa.plus.domain.Notice;
+import com.fa.plus.service.NoticeService;
 
 @Controller
 @RequestMapping("/notice/*")
 public class NoticeController {
+	@Autowired
+	private NoticeService service;
 	
-	@GetMapping("list")
-	public String qnaForm() {
+	@Autowired
+	private MyUtil myUtil;
+	
+	@Autowired
+	private FileManager fileManager;
+
+	
+	@RequestMapping("list")
+	public String list(@RequestParam(value = "page", defaultValue = "1") int current_page,
+			@RequestParam(defaultValue = "all") String schType,
+			@RequestParam(defaultValue = "") String kwd,
+			HttpServletRequest req,
+			Model model)throws Exception {
+		
+		int size = 10; 
+		int total_page = 0;
+		int dataCount = 0;
+		
+		if (req.getMethod().equalsIgnoreCase("GET")) { // GET 방식인 경우
+			kwd = URLDecoder.decode(kwd, "utf-8");
+		}
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("schType", schType);
+		map.put("kwd", kwd);
+		
+		dataCount = service.dataCount(map);
+		if (dataCount != 0) {
+			total_page = myUtil.pageCount(dataCount, size);
+		}
+		
+		if (total_page < current_page) {
+			current_page = total_page;
+		}
+		
+		List<Notice> noticeList = null;
+		if (current_page == 1) {
+			noticeList = service.listNoticeTop();
+		}
+		
+		int offset = (current_page - 1) * size;
+		if(offset < 0) offset = 0;
+
+		map.put("offset", offset);
+		map.put("size", size);
+		
+		List<Notice> list = service.listNotice(map);
+		
+		Date endDate = new Date();
+		long gap;
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		if (list != null) {
+		 for (Notice dto : list) {
+				
+			Date beginDate = formatter.parse(dto.getNotice_reg_date());
+				
+			gap = (endDate.getTime() - beginDate.getTime()) / (60 * 60 * 1000);
+			// dto.어쩌구
+				
+			dto.setNotice_reg_date(dto.getNotice_reg_date().substring(0, 10));
+		 }
+		}		
+		
+		String cp = req.getContextPath();
+		String query = "";
+		String listUrl = cp + "/notice/list";
+		String articleUrl = cp + "/notice/article?page=" + current_page;
+		if (kwd.length() != 0) {
+			query = "schType=" + schType + "&kwd=" + URLEncoder.encode(kwd, "utf-8");
+		}
+		
+		if (query.length() != 0) {
+			listUrl = cp + "/notice/list?" + query;
+			articleUrl = cp + "/notice/article?page=" + current_page + "&" + query;
+		}
+		
+		String paging = myUtil.paging(current_page, total_page, listUrl);
+
+		model.addAttribute("noticeList", noticeList);
+		model.addAttribute("list", list);
+		model.addAttribute("page", current_page);
+		model.addAttribute("dataCount", dataCount);
+		model.addAttribute("size", size);
+		model.addAttribute("total_page", total_page);
+		model.addAttribute("paging", paging);
+		model.addAttribute("articleUrl", articleUrl);
+
+		model.addAttribute("schType", schType);
+		model.addAttribute("kwd", kwd);
+
 		
 		return ".notice.list";
 	}
+	@GetMapping("article")
+	public String article(@RequestParam long num,
+			@RequestParam String page,
+			@RequestParam(defaultValue = "all") String schType,
+			@RequestParam(defaultValue = "") String kwd,
+			Model model) throws Exception {
+		
+		kwd = URLDecoder.decode(kwd, "utf-8");
+		
+		String query = "page=" + page;
+		if (kwd.length() != 0) {
+			query += "&schType=" + schType + "&kwd=" + URLEncoder.encode(kwd, "UTF-8");
+		}
+		
+		service.updateHitCount(num);
+		
+		Notice dto = service.findById(num);
+		if (dto == null) {
+			return "redirect:/notice/list?" + query;
+		}
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("schType", schType);
+		map.put("kwd", kwd);
+		map.put("num", num);
+
+		Notice prevDto = service.findByPrev(map);
+		Notice nextDto = service.findByNext(map);
+		
+		List<Notice> listFile = service.listNoticeFile(num);
+
+		model.addAttribute("dto", dto);
+		model.addAttribute("prevDto", prevDto);
+		model.addAttribute("nextDto", nextDto);
+		model.addAttribute("listFile", listFile);
+		model.addAttribute("page", page);
+		model.addAttribute("query", query);
+
+		return ".notice.article";
+	}
+	
+	public String download() throws Exception {
+		return null;
+	}
+	
+	public String zipdownload() throws Exception{
+		
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- domestic_mountain_project/src/main/java/com/fa/plus/domain/Notice.java
+++ domestic_mountain_project/src/main/java/com/fa/plus/domain/Notice.java
@@ -1,5 +1,127 @@
 package com.fa.plus.domain;
 
 public class Notice {
-
+    private int notice_category_num;
+    private String category_name;
+    private int category_reg_id;	// idx로 번호로 넘겨져서 number
+    private String category_reg_date;
+    private String category_update_date;
+    private int category_order;
+    private int category_visible;
+    
+    private int notice_name;
+    // private int notice_category_num;
+    private String notice_title;
+    private String notice_content;
+    private int notice_hit_count;
+    private String notice_reg_id;
+    private String notice_reg_date;
+    private String notice_update_id;
+    private String notice_update_date;
+    private int notice_visible;
+    private String notice_invisible_date;
+    
+	public int getNotice_category_num() {
+		return notice_category_num;
+	}
+	public void setNotice_category_num(int notice_category_num) {
+		this.notice_category_num = notice_category_num;
+	}
+	public String getCategory_name() {
+		return category_name;
+	}
+	public void setCategory_name(String category_name) {
+		this.category_name = category_name;
+	}
+	public int getCategory_reg_id() {
+		return category_reg_id;
+	}
+	public void setCategory_reg_id(int category_reg_id) {
+		this.category_reg_id = category_reg_id;
+	}
+	public String getCategory_reg_date() {
+		return category_reg_date;
+	}
+	public void setCategory_reg_date(String category_reg_date) {
+		this.category_reg_date = category_reg_date;
+	}
+	public String getCategory_update_date() {
+		return category_update_date;
+	}
+	public void setCategory_update_date(String category_update_date) {
+		this.category_update_date = category_update_date;
+	}
+	public int getCategory_order() {
+		return category_order;
+	}
+	public void setCategory_order(int category_order) {
+		this.category_order = category_order;
+	}
+	public int getCategory_visible() {
+		return category_visible;
+	}
+	public void setCategory_visible(int category_visible) {
+		this.category_visible = category_visible;
+	}
+	public int getNotice_name() {
+		return notice_name;
+	}
+	public void setNotice_name(int notice_name) {
+		this.notice_name = notice_name;
+	}
+	public String getNotice_title() {
+		return notice_title;
+	}
+	public void setNotice_title(String notice_title) {
+		this.notice_title = notice_title;
+	}
+	public String getNotice_content() {
+		return notice_content;
+	}
+	public void setNotice_content(String notice_content) {
+		this.notice_content = notice_content;
+	}
+	public int getNotice_hit_count() {
+		return notice_hit_count;
+	}
+	public void setNotice_hit_count(int notice_hit_count) {
+		this.notice_hit_count = notice_hit_count;
+	}
+	public String getNotice_reg_id() {
+		return notice_reg_id;
+	}
+	public void setNotice_reg_id(String notice_reg_id) {
+		this.notice_reg_id = notice_reg_id;
+	}
+	public String getNotice_reg_date() {
+		return notice_reg_date;
+	}
+	public void setNotice_reg_date(String notice_reg_date) {
+		this.notice_reg_date = notice_reg_date;
+	}
+	public String getNotice_update_id() {
+		return notice_update_id;
+	}
+	public void setNotice_update_id(String notice_update_id) {
+		this.notice_update_id = notice_update_id;
+	}
+	public String getNotice_update_date() {
+		return notice_update_date;
+	}
+	public void setNotice_update_date(String notice_update_date) {
+		this.notice_update_date = notice_update_date;
+	}
+	public int getNotice_visible() {
+		return notice_visible;
+	}
+	public void setNotice_visible(int notice_visible) {
+		this.notice_visible = notice_visible;
+	}
+	public String getNotice_invisible_date() {
+		return notice_invisible_date;
+	}
+	public void setNotice_invisible_date(String notice_invisible_date) {
+		this.notice_invisible_date = notice_invisible_date;
+	}
+    
 }
