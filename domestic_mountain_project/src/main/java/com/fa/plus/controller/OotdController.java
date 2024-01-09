040fa19c9c06f9307a9a642f36721334cc090df8
4reoi <4reoi@DESKTOP-NCAL65T>
2024-01-09 17:36:52 +0900

    ootd 빠진거 추가 - 김혁

--- domestic_mountain_project/src/main/java/com/fa/plus/controller/OotdController.java
+++ domestic_mountain_project/src/main/java/com/fa/plus/controller/OotdController.java
@@ -1,8 +1,13 @@
 package com.fa.plus.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("ootd/*")
@@ -13,5 +18,58 @@ public class OotdController {
 		
 		return ".ootd.list";
 	}
+	
+	@GetMapping("write")
+	public String writeForm(Model model) throws Exception {
+	
+		return null;
+	}
+	
+	@PostMapping("write")
+	public String writeSubmit() throws Exception {
+		
+		return null;
+	}
+	
+	@GetMapping("article")
+	public String article() throws Exception {
+		
+		return null;
+	}
+	
+	@GetMapping("update")
+	public String updateForm() throws Exception {
+		
+		return null;
+	}
+	
+	@PostMapping("update")
+	public String updateSubmit() throws Exception {
+	
+		return null;
+	}
+	
+	@GetMapping("delete")
+	public String delete() throws Exception {
+		
+		return null;
+	}
+	
+	// 댓글 리스트 : AJAX-TEXT
+	@GetMapping("listReply")
+	public String listReply() throws Exception {
+			
+		return null;
+		}
+		
+	@PostMapping("insertBoardLike")
+	@ResponseBody
+	public Map<String, Object> insertBoardLike() throws Exception {
+		
+		return null;
+	}
+		
+		
+		
 }
 
