d9a2c7f9834f120cb6026e5a44bc0553f68ea300
4reoi <4reoi@DESKTOP-NCAL65T>
2024-01-19 15:46:23 +0900

    회원 비밀번호찾기 - 김혁

--- domestic_mountain_project/src/main/java/com/fa/plus/controller/MailController.java
+++ domestic_mountain_project/src/main/java/com/fa/plus/controller/MailController.java
@@ -1,4 +1,4 @@
-package com.fa.plus.mail;
+package com.fa.plus.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.fa.plus.mail.Mail;
+import com.fa.plus.mail.MailSender;
 
 @Controller("mail.mailController")
 @RequestMapping("/mail/*")
@@ -33,6 +36,7 @@ public class MailController {
 		} else {
 			msg+="메일을 전송하는데 실패했습니다.";
 		}
+		
 		
 		reAttr.addFlashAttribute("message", msg);
 		
